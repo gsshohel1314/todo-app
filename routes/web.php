e88ad4fd3c8d2b75411239c15999aa82ad17ec87
Shohel Rana <gsshohel1314@gmail.com>
2025-02-10 21:44:08 +0600

    Tailwind css integrated

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
 
 Route::resource('todos', TodoController::class);
