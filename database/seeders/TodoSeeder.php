c475eee1bdecd1e68309fdbcc389ce75ca92a6f6
Shohel Rana <gsshohel1314@gmail.com>
2025-02-10 20:05:40 +0600

    Todo seeder file added

---
+++ database/seeders/TodoSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Todo;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+        
+        foreach (range(1, 10) as $index) {
+            Todo::create([
+                'user_id' => $faker->randomElement([1, 2]),
+                'title' => $faker->sentence,
+                'description' => $faker->paragraph,
+                'due_time' => $faker->dateTimeBetween('now', '+1 month'),
+                'is_done' => $faker->boolean,
+                'is_email_sent' => $faker->boolean,
+            ]);
+        }
+    }
+}
