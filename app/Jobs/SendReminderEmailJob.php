120d4e32231a14dcddea440a417f66d86db1cf03
Shohel Rana <gsshohel1314@gmail.com>
2025-02-11 06:29:01 +0600

    Send email job setup

---
+++ app/Jobs/SendReminderEmailJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Todo;
+use App\Mail\ReminderEmail;
+use App\Services\CSVService;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendReminderEmailJob implements ShouldQueue
+{
+    use Queueable, Dispatchable, InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Todo $todo){}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // Generate CSV file by using the CSVService
+        $csvFileName = CSVService::generateTodoCSV($this->todo);
+
+        // logger()->info("CSV file generated: {$csvFileName}");
+
+        // Send the reminder email
+        Mail::to($this->todo->user->email)->send(new ReminderEmail($this->todo, $csvFileName));
+
+        // Mark email as sent
+        $this->todo->update(['is_email_sent' => true]);
+
+        logger()->info("Reminder email sent for Todo: {$this->todo->title}");
+    }
+}
